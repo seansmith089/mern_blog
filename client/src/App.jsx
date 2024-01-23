872d52e2d3ea516a381f89269b0b32fd44bfde43
Sean Smith <seansmith089@gmail.com>
2024-01-23 13:05:02 +1100

    added navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,20 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 // Routes Import
-
 import Home from "./pages/Home"
 import About from "./pages/About"
 import Dashboard from "./pages/Dashboard"
 import Projects from "./pages/Projects"
 import SignIn from "./pages/SignIn"
 import SignUp from "./pages/SignUp"
+import Header from "./components/Header"
 
 function App() {
 
   return (
     <>
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />}/> 
                 <Route path="/about" element={<About />}/> 
