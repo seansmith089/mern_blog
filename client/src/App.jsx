c20528e293d53b718e2ec62a4462bb3f5f103b4d
Sean Smith <seansmith089@gmail.com>
2024-01-23 11:54:20 +1100

    Created pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,29 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+// Routes Import
+
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Dashboard from "./pages/Dashboard"
+import Projects from "./pages/Projects"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
 
 function App() {
 
   return (
     <>
-        <h1 className="text-red-400">hello</h1>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />}/> 
+                <Route path="/about" element={<About />}/> 
+                <Route path="dashboard" element={<Dashboard />}/> 
+                <Route path="projects" element={<Projects />}/> 
+                <Route path="signin" element={<SignIn />}/> 
+                <Route path="signup" element={<SignUp />}/> 
+            </Routes>
+        </BrowserRouter>
+        
     </>
   )
 }
