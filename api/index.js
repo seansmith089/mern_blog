d58ca6b80b66ed38a9ea550b27545c9cec3975f9
Sean Smith <seansmith089@gmail.com>
2024-02-07 17:31:45 +1100

    added comment functionality

--- api/index.js
+++ api/index.js
@@ -1,41 +1,52 @@
 import express from "express";
 import mongoose from "mongoose";
-import dotenv from "dotenv"
-import userRoutes from "./routes/user.routes.js"
-import authRoutes from "./routes/auth.routes.js"
-import postRoutes from "./routes/post.routes.js"
-import cookieParser from "cookie-parser"
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.routes.js";
+import authRoutes from "./routes/auth.routes.js";
+import postRoutes from "./routes/post.routes.js";
+import commentRoutes from "./routes/comment.routes.js";
+import cookieParser from "cookie-parser";
+import path from "path";
 
-dotenv.config()
+dotenv.config();
 
-const app = express();
-app.use(express.json())
-app.use(cookieParser())
-
-
-
-// Connect to MongoDB and start server
 mongoose
   .connect(process.env.MONGODB)
   .then(() => {
-    console.log("MongoDB Connected!");
-    app.listen(3000, () => console.log("Server Running"));
+    console.log("MongoDb is connected");
   })
-  .catch((error) => {
-    console.log(error);
+  .catch((err) => {
+    console.log(err);
   });
 
+const __dirname = path.resolve();
 
-app.use("/api/user", userRoutes )
-app.use("/api/auth", authRoutes )
-app.use("/api/post", postRoutes )
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000!");
+});
+
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/post", postRoutes);
+app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use((err, req, res, next) => {
-    const statusCode = err.statusCode || 500
-    const message = err.message || "internal server error"
-    res.status(statusCode).json({
-        success: false,
-        statusCode, 
-        message
-    })
-})
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,49 +1,43 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
+import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import PrivateRoute from "./components/PrivateRoute";
+import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute";
+import CreatePost from "./pages/CreatePost";
+import UpdatePost from "./pages/UpdatePost";
+import PostPage from "./pages/PostPage";
+import ScrollToTop from "./components/ScrollToTop";
+import Search from "./pages/Search";
 
-// Routes Import
-import Home from "./pages/Home"
-import About from "./pages/About"
-import Dashboard from "./pages/Dashboard"
-import Projects from "./pages/Projects"
-import SignIn from "./pages/SignIn"
-import SignUp from "./pages/SignUp"
-import Header from "./components/Header"
-import FooterComponent from "./components/Footer"
-import PrivateRoute from "./components/PrivateRoute"
-import OnlyAdminPrivateRoute from "./components/OnlyAdminPrivateRoute"
-import CreatePost from "./pages/CreatePost"
-import UpdatePost from "./pages/UpdatePost"
-import PostPage from "./pages/PostPage"
-
-function App() {
-
+export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/signin" element={<SignIn />} />
-          <Route path="/signup" element={<SignUp />} />
+    <BrowserRouter>
+      <ScrollToTop />
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/search" element={<Search />} />
+        <Route element={<PrivateRoute />}>
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Route>
+        <Route element={<OnlyAdminPrivateRoute />}>
+          <Route path="/create-post" element={<CreatePost />} />
+          <Route path="/update-post/:postId" element={<UpdatePost />} />
+        </Route>
 
-            {/* Only for authenticated users */}
-          <Route element={<PrivateRoute />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-          </Route>
-            {/* Only for admin users */}
-          <Route element={<OnlyAdminPrivateRoute />}>
-            <Route path="/create-post" element={<CreatePost />} />
-            <Route path="/update-post/:postId" element={<UpdatePost />} />
-          </Route>
-
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/post/:postSlug" element={<PostPage />} />
-        </Routes>
-        <FooterComponent />
-      </BrowserRouter>
-    </>
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/post/:postSlug" element={<PostPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
-
-export default App
